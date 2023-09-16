38002a94e2d926420c3615295c190a7dcd7f557f
Mfundo-debug <diditmfundo@gmail.com>
2023-09-17 00:24:41 +0200

    footer of the page

---
+++ credit_system/system-front/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer style={{backgroundColor: 'lightblue', textAlign: 'center', padding: '10px'}}>
+            <p>Contact: <a href="mailto:diditmfundo@gmail.com">diditmfundo@gmail.com</a> </p>
+        </footer>
+    );
+};
+export default Footer;
