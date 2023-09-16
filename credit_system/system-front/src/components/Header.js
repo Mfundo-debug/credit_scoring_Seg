6da873c77837459a4703ba5366477a53de34df9e
Mfundo-debug <diditmfundo@gmail.com>
2023-09-17 00:24:51 +0200

    Header of the page

---
+++ credit_system/system-front/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Header = () => {
+    return (
+        <header style={{backgroundColor: 'lightblue', textAlign: 'center', padding: '10px'}}>
+            <h1>Credit Scoring and Segmentation System</h1>
+        </header>
+    );
+};
+export default Header;
