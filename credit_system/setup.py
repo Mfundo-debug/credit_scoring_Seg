55a6867235ff0c701095b99b7152e663c82f6708
Mfundo-debug <diditmfundo@gmail.com>
2023-09-17 12:12:37 +0200

    created setup file to package

---
+++ credit_system/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='credit_system',
+    version='0.1',
+    description='Credit Score and Loan Approval System',
+    author='Mfundo Monchwe',
+    author_email='diditmfundo@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires= [
+        'django==4.2.5',
+        'djangorestframework==3.14.0',
+        'pandas==2.0.2',
+        'numpy==1.24.3',
+        'matplotlib==3.7.1',
+        'scikit-learn==1.2.2',
+        'seaborn==0.12.2',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.11.4',
+        'Framework :: Django :: 4.2.5', 
+        'Intended Audience :: Developers/Data Scientist',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    entry_points={
+        'console_scripts': [
+            'credit_system = credit_system.manage:main',
+        ],
+    },
+)
