a22b9b177ef8e32589cffaf3773ff31f033113f3
Mfundo-debug <diditmfundo@gmail.com>
2023-09-18 03:14:51 +0200

    added one lib

--- credit_system/setup.py
+++ credit_system/setup.py
@@ -11,6 +11,7 @@ setup(
     install_requires= [
         'django==4.2.5',
         'djangorestframework==3.14.0',
+        'django-cors-headers==4.2.0',
         'pandas==2.0.2',
         'numpy==1.24.3',
         'matplotlib==3.7.1',
